634d91363c8a42c714f77b163175a747ff6e7988
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2025-01-02 15:17:23 -0800

    Update NEMP25_GEX_preprocessing.py

--- NEMP25_GEX_preprocessing.py
+++ NEMP25_GEX_preprocessing.py
@@ -18,6 +18,7 @@ anndata_initial = directory_path + '/NEMP25_initial.h5ad'
 adata = sc.read_h5ad(anndata_initial)
 
 #Define QC cutoffs.
+print('Filtering the dataset...')
 n_genes_cutoff = 9000
 total_counts_cutoff = 30000
 mito_cutoff = 20
@@ -41,34 +42,19 @@ adata = adata[adata.obs.total_counts < total_counts_cutoff, :]
 adata = adata[adata.obs.pct_counts_mito < mito_cutoff, :]
 adata = adata[adata.obs.pct_counts_ribo < ribo_cutoff, :]
 
-#Normalize to median total counts and logarithmize.
-print('Normalizing and logarithmizing')
+#Preprocess the dataset.
+print('Preprocessing the dataset...')
 sc.pp.normalize_total(adata)
 sc.pp.log1p(adata)
-
-#Identify and plot highly variable genes from each AnnData object as an H5AD file.
-print('Finding highly variable genes')
 sc.pp.highly_variable_genes(adata, n_top_genes=2000, batch_key="GEMwell", flavor='seurat')
 sc.pl.highly_variable_genes(adata, save=f'{save_name}.png')
-
-#Perform PCA dimensional reduction.
-print('Running PCA')
 sc.tl.pca(adata, svd_solver='arpack')
-
-#Compute the nearest neighbors using the knn algorithm.
-print('Running neighbor finding')
 sc.pp.neighbors(adata)
-
-#Compute the umap embedding based on knn-computed neighbors.
-print('Running UMAP')
 sc.tl.umap(adata)
-
-# Cluster umap embeddings using leiden
-print('Leiden clustering')
 res = 1
 sc.tl.leiden(adata, flavor="igraph", n_iterations=2, resolution=res)
 
-#Save the AnnData object as an H5AD file.
+#Save the results.
 print('Saving preprocessed AnnData object')
 anndata_preprocessed = directory_path + '/NEMP25_preprocessed.h5ad'
 adata.write(anndata_preprocessed, compression='gzip')
