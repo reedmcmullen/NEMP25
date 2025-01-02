ba18742e42510e84fc1d0b3f805a345a2c7b3ef5
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2025-01-02 13:44:54 -0800

    Update NEMP25_GEX_preprocessing.py

--- NEMP25_GEX_preprocessing.py
+++ NEMP25_GEX_preprocessing.py
@@ -1,5 +1,20 @@
+#! /usr/bin/env python3
+#Import required packages and modules.
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+import matplotlib.pyplot as plt
+import os
+import numpy as np
 
+#Define variables and settings.
+directory_path = '/wynton/home/pollenlab/reedmcmullen/projects/NEMP25/scanpy_NEMP25'
+os.chdir(directory_path)
+save_name = "NEMP25"
 
+#Load dataset.
+print('Loading the dataset...')
+anndata_initial = directory_path + '/
 
 #Normalize to median total counts and logarithmize.
 print('Normalizing and logarithmizing')
