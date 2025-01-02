472463858ef3ecd194b3a7ce5fcacc5cf5631629
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2025-01-02 15:15:05 -0800

    Update NEMP25_GEX_preprocessing.py

--- NEMP25_GEX_preprocessing.py
+++ NEMP25_GEX_preprocessing.py
@@ -17,6 +17,30 @@ print('Loading the dataset...')
 anndata_initial = directory_path + '/NEMP25_initial.h5ad'
 adata = sc.read_h5ad(anndata_initial)
 
+#Define QC cutoffs.
+n_genes_cutoff = 9000
+total_counts_cutoff = 30000
+mito_cutoff = 20
+ribo_cutoff = 10
+
+#Plot violin plots of QC metrics with cutoffs.
+fig, axes = plt.subplots(1, 4, figsize=(16, 6))
+sc.pl.violin(adata, 'n_genes_by_counts', ax=axes[0], stripplot=False, show=False)
+axes[0].axhline(n_genes_cutoff, color='red', linestyle='--')
+sc.pl.violin(adata, 'total_counts', ax=axes[1], stripplot=False, show=False)
+axes[1].axhline(total_counts_cutoff, color='red', linestyle='--')
+sc.pl.violin(adata, 'pct_counts_mito', ax=axes[2], stripplot=False, show=False)
+axes[2].axhline(mito_cutoff, color='red', linestyle='--')
+sc.pl.violin(adata, 'pct_counts_ribo', ax=axes[3], stripplot=False, show=False)
+axes[3].axhline(ribo_cutoff, color='red', linestyle='--')
+plt.savefig(os.path.join(directory_path, 'figures', f'_{save_name}_qc_metrics_cutoffs.png'), bbox_inches='tight')
+
+#Filter outlier cells from each AnnData object based on QC metric plots.
+adata = adata[adata.obs.n_genes_by_counts < n_genes_cutoff, :]
+adata = adata[adata.obs.total_counts < total_counts_cutoff, :]
+adata = adata[adata.obs.pct_counts_mito < mito_cutoff, :]
+adata = adata[adata.obs.pct_counts_ribo < ribo_cutoff, :]
+
 #Normalize to median total counts and logarithmize.
 print('Normalizing and logarithmizing')
 sc.pp.normalize_total(adata)
