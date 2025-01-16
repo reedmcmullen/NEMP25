25c05a7997f706d1585f0f03f92e08ab91adbde5
reedmcmullen <54691534+reedmcmullen@users.noreply.github.com>
2025-01-16 12:35:39 -0800

    Update edist_NEMP25.py

--- edist_NEMP25.py
+++ edist_NEMP25.py
@@ -16,12 +16,19 @@ print('Loading dataset...')
 anndata = '/wynton/group/pollen/reedmcmullen/projects/NEMP25/scanpy_NEMP25/NEMP25_Tel.h5ad'
 adata = sc.read_h5ad(anndata)
 
-#Subsetting and formatting dataset.
+#Subset dataset.
 print('Subsetting and formatting dataset...')
 adata = adata[adata.obs['cell_type'].isin(['Tel_NE', 'Tel_NE_ventral', 'Tel_NE_dorsal'])]
 adata = adata[adata.obs['species'].isin(['Human', 'Chimp'])]
+
+#Format dataset.
 adata.obs['perturbation'] = adata.obs['perturbation'].str.replace(r'\(.*?\)', '', regex=True) #Remove parentheses and any characters between them.
+adata.obs['perturbation'] = adata.obs['perturbation'].str.replace('5%', '', regex=False) #Remove 5% from 5%TreMan value of perturbation column.
 adata.obs['group'] = adata.obs['species'].astype(str) +'_'+ adata.obs['time_point'].astype(str) +'_'+ adata.obs['perturbation'].astype(str)
+adata.obs['simple_group'] = adata.obs['species'].astype(str) +'_'+ adata.obs['perturbation'].astype(str)
+adata.obs['group'] = adata.obs['group'].astype(str)
+adata.obs['simple_group'] = adata.obs['simple_group'].astype(str)
+adata.obs['cell_type'] = adata.obs['cell_type'].astype(str)
 
 #Calculate energy distance between perturbations.
 print('Calculating and plotting energy distance between perturbations...')
@@ -33,13 +40,22 @@ pert_edist_df.to_csv(directory_path + '/pert_edist_df.csv')
 clustermap(pert_edist_df, robust=True, figsize=(20, 20))
 plt.savefig(os.path.join(directory_path, 'perturbation_edist_clustermap.png'), bbox_inches='tight')
 
+#Calculate energy distance between simple groups.
+print('Calculating and plotting energy distance between simple groups...')
+distance = pt.tl.Distance("edistance", obsm_key="X_pca")
+simple_group_edist_df = distance.pairwise(adata, groupby='simple_group')
+simple_group_edist_df.to_csv(directory_path + '/simple_group_edist_df.csv')
+
+#Plot energy distance between groups.
+clustermap(simple_group_edist_df, robust=True, figsize=(20, 20))
+plt.savefig(os.path.join(directory_path, 'simple_group_edist_clustermap.png'), bbox_inches='tight')
+
 #Calculate energy distance between groups.
 print('Calculating and plotting energy distance between groups...')
 distance = pt.tl.Distance("edistance", obsm_key="X_pca")
 group_edist_df = distance.pairwise(adata, groupby='group')
 group_edist_df.to_csv(directory_path + '/group_edist_df.csv')
 
-#Plot energy distance between perturbations.
+#Plot energy distance between groups.
 clustermap(group_edist_df, robust=True, figsize=(20, 20))
 plt.savefig(os.path.join(directory_path, 'group_edist_clustermap.png'), bbox_inches='tight')
-
